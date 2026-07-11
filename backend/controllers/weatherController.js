ff1c00eb592472b1f9e857b0e3de678aeea6d093
Satwik367 <satwikmaiti59@gmail.com>
2026-07-11 22:19:09 +0530

    updated

--- backend/controllers/weatherController.js
+++ backend/controllers/weatherController.js
@@ -7,54 +7,45 @@ const getWeather = async (req, res) => {
         const { city } = req.query;
 
         if (!city) {
-
             return res.status(400).json({
                 success: false,
                 message: "City is required"
             });
-
         }
 
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.WEATHER_API_KEY}&units=metric`;
-
-        const response = await axios.get(url);
+        const response = await axios.get(
+            "https://api.openweathermap.org/data/2.5/weather",
+            {
+                params: {
+                    q: city,
+                    appid: process.env.WEATHER_API_KEY,
+                    units: "metric"
+                }
+            }
+        );
 
         const data = response.data;
 
         res.json({
-
             success: true,
-
             weather: {
-
                 city: data.name,
-
                 country: data.sys.country,
-
                 temperature: data.main.temp,
-
                 humidity: data.main.humidity,
-
                 feelsLike: data.main.feels_like,
-
                 description: data.weather[0].description,
-
                 windSpeed: data.wind.speed
-
             }
-
         });
 
-    }
+    } catch (err) {
 
-    catch (err) {
+        console.log(err.response?.data || err.message);
 
         res.status(500).json({
-
             success: false,
-
-            message: "Unable to fetch weather"
-
+            error: err.response?.data || err.message
         });
 
     }
@@ -62,7 +53,5 @@ const getWeather = async (req, res) => {
 };
 
 module.exports = {
-
     getWeather
-
 };
